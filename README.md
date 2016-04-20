f996c51e195a841bb601abaac69353842c30700b
yl2526 <yl2526@cornell.edu>
2016-04-20 14:29:10 -0400

    changed option -t and --top=

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ this simple sript will goole the arguments and open the top five search result.
 **python quickgoogle.py hehe wahaha**
 will google 'hehe wahaha' and open the top five results  
   
-**python quickgoogle.py --top n hehe wahaha**
-will google 'hehe wahaha' and open the top n results and n must be an integer.
+Also there are two argument to make the number of search results to open: **-t n and --top=n**. 
+The options must be typed before the search keyword, otherwise the getopt will 
+not be able to capture it.  
 
-Also the **shebang line** only works on **linux**.
+Also the **shebang line** only works on **linux**. No idea about how to do it on windows.
--- quickgoogle.py
+++ quickgoogle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# so far the shebang line only works for linux. 
+# I hope I can find a way to make it owrk on windows.
+
 """
 google something and open the top five search results
 """
@@ -8,16 +11,22 @@ google something and open the top five search results
 import requests
 import webbrowser
 import bs4
-import sys
+import sys, getopt
 
-if (len(sys.argv) >= 3) & (sys.argv[1] == '--top'):
-    top_num = int(sys.argv[2])
-    search_keyword = ' '.join(sys.argv[3:])
+# -t 5 and --top=5 are two ways to specify the number of top search results to open
+opts, args = getopt.getopt(sys.argv[1:], "t:", ["top="])
+
+if opts:
+    opt, val = zip(*opts)
+    assert all([op in ('-t', '--top') for op in opt]), 'unknow options' 
+    # in case that both -t and --top= are used, only the smaller one will be used
+    top_num = min([int(v) for v in val])
 else:
     top_num = 5
-    search_keyword = ' '.join(sys.argv[1:])
-        
 
+search_keyword = ' '.join(args)  
+
+#open the search page
 search_link = 'http://google.com/search?q=' + search_keyword
 webbrowser.open(search_link)
 
@@ -26,7 +35,12 @@ soup = bs4.BeautifulSoup(res.text)
 
 link_elems = soup.select('.r a')
 
+# in case that there are only a few search results, 
+# take the smaller of links found and specifiedd number
+
 num_open = min(top_num, len(link_elems))
 
 for i in range(num_open):
+    #open the top search results' links
     webbrowser.open('http://google.com' + link_elems[i].get('href'))
+
